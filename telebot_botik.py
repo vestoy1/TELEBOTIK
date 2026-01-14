494a46b777acd288321140ff577da9ddc554206d
Roma Pogorelov <romik125125@gmail.com>
2026-01-14 17:37:23 +0500

    elebot_botik.py

--- telebot_botik.py
+++ telebot_botik.py
@@ -1,77 +1,17 @@
 import telebot
 from telebot.types import ReactionTypeEmoji
 import random
-from bot_logic import gen_pass, flip_coin
+import os
+
 bot = telebot.TeleBot("8554078106:AAGOcgYMxS3YK_NPPmQ3wEPVvlz-W2yFS5w")
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+@bot.message_handler(commands=['mem'])
+def send_mem(message):
+    imgs = os.listdir(r'C:\Users\Ирина\проекты VS code\meme_bot\images')
+    bot.reply_to(message, imgs)
+    print(imgs)
+    with open(fr'C:\Users\Ирина\проекты VS code\meme_bot\images\{random.choice(imgs)}', 'rb') as f:
+        bot.send_photo(message.chat.id, f)
     
-@bot.message_handler(commands=['hello'])
-def send_hello(message):
-    bot.reply_to(message, "Привет! Как дела?")
-    
-@bot.message_handler(commands=['bye'])
-def send_bye(message):
-    bot.reply_to(message, "Пока! Удачи!")
-    
-    
-# @bot.message_handler(func=lambda message: True)
-# def echo_all(message):
-#     bot.reply_to(message, message.text)
-    
-@bot.message_handler(commands=["genpass"])
-def gen_pass1(message):
-    password = gen_pass(10)
-    bot.reply_to(message, password)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
-@bot.message_handler(commands=['coin'])
-def send_coin(message):
-    coin = flip_coin()
-    bot.reply_to(message, f"Монетка выпала так: {coin}")
-
-@bot.message_handler(commands=['heh'])
-def send_heh(message):
-    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
-    bot.reply_to(message, "he" * count_heh)
-
-@bot.message_handler(func=lambda message: True)
-def send_reaction(message):
-    emo = ["\U0001F525", "\U0001F917", "\U0001F60E"]  # or use ["🔥", "🤗", "😎"]
-    bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji(random.choice(emo))], is_big=False)
-
-#
-
-@bot.message_handler(commands=["poll"])
-def create_poll(message):
-    bot.send_message(message.chat.id, "English Article Test")
-    answer_options = ["a", "an", "the", "-"]
-
-    bot.send_poll(
-        chat_id=message.chat.id,
-        question="We are going to '' park.",
-        options=answer_options,
-        type="quiz",
-        correct_option_id=2,
-        is_anonymous=False,
-    )
-
-
-@bot.poll_answer_handler()
-def handle_poll(poll):
-    # This handler can be used to log User answers and to send next poll
-    pass
-
-
-
-
-
-
-
-bot.infinity_polling()
-bot.polling()
+bot.polling()  
